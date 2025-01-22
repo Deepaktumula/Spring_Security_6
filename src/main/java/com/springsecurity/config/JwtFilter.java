0864b1c5e75696917eac1c2de0a1005a6a2a9eb8
Tumula Deepak <94121922+Deepaktumula@users.noreply.github.com>
2025-01-22 23:19:42 +0530

    handled exception in JwtFilter.java

--- src/main/java/com/springsecurity/config/JwtFilter.java
+++ src/main/java/com/springsecurity/config/JwtFilter.java
@@ -14,6 +14,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import com.springsecurity.service.JwtService;
 import com.springsecurity.service.MyUserDetailsService;
 
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -35,27 +36,37 @@ public class JwtFilter extends OncePerRequestFilter {
 		String authHeader = request.getHeader("Authorization");
 		String token = null;
 		String userName = null;
+		try {
 
-		if (authHeader != null && authHeader.startsWith("Bearer ")) {
-//			Extracting only token
-			token = authHeader.substring(7);
-//			Extracting UserName
-			userName = jwtService.extractUserName(token);
+			if (authHeader != null && authHeader.startsWith("Bearer ")) {
+//				Extracting only token
+				token = authHeader.substring(7);
+//				Extracting UserName
+				userName = jwtService.extractUserName(token);
 
-		}
-//		Checking whether the UserName is present or not and checking if it is already authenticated before or not
-		if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-//			Getting the User Details from this
-			UserDetails userDetails = context.getBean(MyUserDetailsService.class).loadUserByUsername(userName);
-//			sending token and UserDetails to validate the token
-			if (jwtService.validateToken(token, userDetails)) {
-//				Creating the token for newly logged in User
-				UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
-						null, userDetails.getAuthorities());
-//				Now setting the details inside the Spring Security Context
-				authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-				SecurityContextHolder.getContext().setAuthentication(authToken);
 			}
+//			Checking whether the UserName is present or not and checking if it is already authenticated before or not
+			if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+//				Getting the User Details from this
+				UserDetails userDetails = context.getBean(MyUserDetailsService.class).loadUserByUsername(userName);
+//				sending token and UserDetails to validate the token
+				if (jwtService.validateToken(token, userDetails)) {
+//					Creating the token for newly logged in User
+					UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
+							null, userDetails.getAuthorities());
+//					Now setting the details inside the Spring Security Context
+					authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+					SecurityContextHolder.getContext().setAuthentication(authToken);
+				}
+			}
+		} catch (JwtException e) {
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			response.getWriter().write("Invalid token or Token Expired");
+			return;
+		}catch (Exception e) {
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			response.getWriter().write(e.getMessage());
+			return;
 		}
 //		To continue the Filter chain we need to send request and response object
 		filterChain.doFilter(request, response);
